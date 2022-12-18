748f5de9621ae5cbc7e868370d76ee893986870c
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-12-17 18:16:53 -0600

    button children

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import logo from "./images/logo.png";
+import Button from "./components/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container-logo">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+      <div className="container-calculator">
+        <div className="row">
+          <Button>1</Button>
+        </div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
